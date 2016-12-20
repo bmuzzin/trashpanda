48004d42e1858cc39cd054f948ab3487ef6d5f4e
Christopher Lowenthal <christopher.lowenthal@autodesk.com>
2016-12-20 16:13:22 -0500

    Scaleform UI

---
+++ content/ui/trash_panda.s2d/InputWidgets/Input_handler.lua
@@ -0,0 +1,44 @@
+-- NOTE: This handler object is auto-generated. Feel free to modify the contents of this file.
+--
+-- IMPORTANT: This script resource must be attached to a script component named WidgetHandler to work
+--            with the input widget's base script.
+--
+
+GameState = GameState or {}
+
+local thisActor = ...;
+local actorName = scaleform.Actor.name(thisActor);
+
+-- The handler object expected by the base widget script
+-- If any of the handler methods are not defined, then the base widget will not attempt to invoke them.
+local handler = {
+
+    -- Invoked when the input receives a key down event
+    -- Param KeyCode : Number
+    -- Param KeyState : Number
+    keyDown = function(KeyCode, KeysState)
+        print(actorName .. " input new key down event: [Code:" .. KeyCode .. "]");
+
+        if KeyCode == scaleform.Keys.Return then
+            --TODO click go button
+        end
+        
+
+        -- The code below gives an example of how to dispatch a custom event from this event handler function.
+        --[[
+        local evt = { eventId = scaleform.EventTypes.Custom,
+                      name = actorName .. "_keyDown",
+                      data = { keyCode = KeyCode, keysState = KeysState } }
+        scaleform.Stage.dispatch_event(evt)
+        --]]
+    end
+
+    -- Invoked when the input focus change
+    -- Param focus : boolean
+    ,hasFocus = function(focus)
+        print(actorName .. " input has focus:" .. tostring(focus));
+    end
+
+}
+
+return handler;
